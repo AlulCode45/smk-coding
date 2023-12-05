cc68dd9ff0ab5f2d55af48a52e03913bb1bae23d
Muhammad Khoyron AF <alulgans70@gmail.com>
2023-12-05 21:16:50 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#3a10e5',
+        'young-blue': '#f0f3ff',
+        'yellow': '#facb3b'
+      }
+    },
+  },
+  plugins: [],
+}
